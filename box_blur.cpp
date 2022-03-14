fa52c9c4228c0ebb605684f4763225a9b7d313d5
YuichiKamishiro <yuichikamishiro@gmail.com>
2022-03-15 01:25:30 +0300

    comments update + code update

--- box_blur.cpp
+++ box_blur.cpp
@@ -1,42 +1,60 @@
 //It's a simple BoxBlur without applying on borders
 //Author: YuichiKamishiro
 //Contributors: RoziePlaysPython(https://github.com/RoziePlaysPython)
-#include <SFML/Graphics.hpp>
 
+//How it works? https://en.wikipedia.org/wiki/Box_blur
+//Average of p pixel
+// x x x  1
+// x p x  -
+// x x x  9
+
+#include <SFML/Graphics.hpp>
 #include <SFML/System/Vector2.hpp>
+
 #include <iostream>
+#include <sstream>
 
 using namespace std;
 using namespace sf;
 
+//box_blur
 Image box_blur(Vector2u size, Image img);
-//BoxBlur
 
 int main(int argc, char* argv[]){
+    //loading image
     Image img;
     if(!img.loadFromFile(argv[1])){
         cout << "File was not find, try another argument\n";
         exit(0);
     }
-    //Loading image
+
+    //getting size
     Vector2u size = img.getSize();
     cout << "Size of this image -> x: " << size.x << " y: " << size.y << endl;
-    //Getting Size
-    for(int i = 0;i < 10;i++)
+    
+    //converting second argument to int
+    stringstream sstr;
+    sstr << argv[2];
+    int num;
+    sstr >> num;
+   
+    //how many blur effect will be applied
+    for(int i = 0;i < num;i++)
         img = box_blur(size, img);
-    //How many blur effects will be applied
+    
+    //converting image to sprite
     Sprite spr;
     Texture texture;
     texture.loadFromImage(img);
     spr.setTexture(texture);
-    //Converting image to sprite
-    RenderWindow window(VideoMode(700, 700),"window");
+    
+    //window processing
+    RenderWindow window(VideoMode(img.getSize().x, img.getSize().y),"window");
     while(window.isOpen()){
         window.clear();
         window.draw(spr);
         window.display();
     }
-    //Window processing
 }
 
 Image box_blur(Vector2u size, Image img){
@@ -61,12 +79,13 @@ Image box_blur(Vector2u size, Image img){
             for(int deltaY = -1; deltaY < 2; deltaY++){
                 for(int deltaX = -1; deltaX < 2; deltaX++){
                     a += imgc.getPixel(x + deltaX, y + deltaY).a;}};
-
+            
+            //average of each pixel
             b = b / 9;
             r = r / 9;
             g = g / 9;
             a = a / 9;
-               
+            
             Color color;
             color.r = r;
             color.g = g;
@@ -74,10 +93,6 @@ Image box_blur(Vector2u size, Image img){
             color.a = a;
 
             img.setPixel(x, y, color); 
-            //Average of p pixel
-            // x,x,x  1
-            // x,p,x  -
-            // x,x,x  9
         }
     }
     return img;
