16710d11dfa532c24d1ffdeb4b850e3277160588
RoziePlaysPython <tfatugava@gmail.com>
2022-03-14 12:24:14 +0300

    changed fixed blur box to extensible: now 15x15 blur is possible!

--- box_blur.cpp
+++ box_blur.cpp
@@ -3,15 +3,21 @@
 
 #include <SFML/System/Vector2.hpp>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 using namespace sf;
 
-Image box_blur(Vector2u size, Image img);
+Image box_blur(Vector2u size, Image img, int delta);
 //BoxBlur
 
 int main(int argc, char* argv[]){
     Image img;
+    int delta = atoi(argv[2]); //from the command line
+    if(!(delta % 2 == 1)){
+        cout << "Only odd numbers are accepted as blur delta\n";
+        exit(0);
+    }
     if(!img.loadFromFile(argv[1])){
         cout << "File was not find, try another argument\n";
         exit(0);
@@ -20,8 +26,8 @@ int main(int argc, char* argv[]){
     Vector2u size = img.getSize();
     cout << "Size of this image -> x: " << size.x << " y: " << size.y << endl;
     //Getting Size
-    for(int i = 0;i < 10;i++)
-        img = box_blur(size, img);
+    for(int i = 0;i < 1;i++)
+        img = box_blur(size, img, delta);
     //How many blur effects will be applied
     Sprite spr;
     Texture texture;
@@ -37,33 +43,33 @@ int main(int argc, char* argv[]){
     //Window processing
 }
 
-Image box_blur(Vector2u size, Image img){
-    for(int x = 1;x < size.x - 1;x++){
-        for(int y = 1;y < size.y - 1;y++){
+Image box_blur(Vector2u size, Image img, int delta){
+    cout << "box_blur called\n";
+    for(int x = delta / 2;x < size.x - 1; x++){
+        for(int y = delta / 2;y < size.y - 1; y++){
             int r = 0; int g = 0; int b = 0; int a = 0;
             // 3x3 pixel scanning for red color
-            for(int deltaY = -1; deltaY < 2; deltaY++){
-                for(int deltaX = -1; deltaX < 2; deltaX++){
+            for(int deltaY = -(delta / 2); deltaY <= delta / 2; deltaY++){
+                for(int deltaX = -(delta / 2); deltaX <= delta / 2; deltaX++){
                     r += img.getPixel(x + deltaX, y + deltaY).r;}};
             // 3x3 pixel scanning for green color
-            for(int deltaY = -1; deltaY < 2; deltaY++){
-                for(int deltaX = -1; deltaX < 2; deltaX++){
+            for(int deltaY = -(delta / 2); deltaY <= delta / 2; deltaY++){
+                for(int deltaX = -(delta / 2); deltaX <= delta / 2; deltaX++){
                     g += img.getPixel(x + deltaX, y + deltaY).g;}};
             // 3x3 pixel scanning for blue color
-            for(int deltaY = -1; deltaY < 2; deltaY++){
-                for(int deltaX = -1; deltaX < 2; deltaX++){
+            for(int deltaY = -(delta / 2); deltaY <= delta / 2; deltaY++){
+                for(int deltaX = -(delta / 2); deltaX <= delta / 2; deltaX++){
                     b += img.getPixel(x + deltaX, y + deltaY).b;}};
             // 3x3 pixel scanning for alpha
-            for(int deltaY = -1; deltaY < 2; deltaY++){
-                for(int deltaX = -1; deltaX < 2; deltaX++){
+            for(int deltaY = -(delta / 2); deltaY <= delta / 2; deltaY++){
+                for(int deltaX = -(delta / 2); deltaX <= delta / 2; deltaX++){
                     a += img.getPixel(x + deltaX, y + deltaY).a;}};
               
-            //dividing by 9 to find the average between 9 pixels
-            b = b / 9;
-            r = r / 9;
-            g = g / 9;
-            a = a / 9;
-            
+            //dividing by delta^2 to find the average between 9 pixels
+            b = b / pow(delta, 2);
+            r = r / pow(delta, 2);
+            g = g / pow(delta, 2);
+            a = a / pow(delta, 2);
             //creating color from achieved averages for each r g b a
             Color color;
             color.r = r;
